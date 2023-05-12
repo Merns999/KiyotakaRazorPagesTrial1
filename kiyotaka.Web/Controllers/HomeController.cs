4e75f28009f629c6dbcaa368bc1581149939973b
Mansur Akidah <mansurakidah@gmail.com>
2023-05-12 11:23:51 +0300

    Same

--- kiyotaka.Web/Controllers/HomeController.cs
+++ kiyotaka.Web/Controllers/HomeController.cs
@@ -1,23 +1,23 @@
-﻿using kiyotaka.Web.Data;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+﻿//using kiyotaka.Web.Data;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.EntityFrameworkCore;
 
-namespace kiyotaka.Web.Controllers
-{
-    public class HomeController : Controller
-    {
-        private readonly kiyotakaDbContext _context;
+//namespace kiyotaka.Web.Controllers
+//{
+//    public class HomeController : Controller
+//    {
+//        private readonly kiyotakaDbContext _context;
 
-        public HomeController(kiyotakaDbContext context)
-        {
-            _context = context;
-        }
+//        public HomeController(kiyotakaDbContext context)
+//        {
+//            _context = context;
+//        }
 
-        public async Task<IActionResult> Index()
-        {
-            var animeList = await _context.AnimeHubs.ToListAsync();
+//        public async Task<IActionResult> Index()
+//        {
+//            var animeList = await _context.AnimeHubs.ToListAsync();
 
-            return View(animeList);
-        }
-    }
-}
+//            return View(animeList);
+//        }
+//    }
+//}
--- kiyotaka.Web/Controllers/RolesController.cs
+++ kiyotaka.Web/Controllers/RolesController.cs
@@ -1,36 +1,36 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
+﻿//using Microsoft.AspNetCore.Identity;
+//using Microsoft.AspNetCore.Mvc;
 
-namespace kiyotaka.Web.Controllers
-{
-    public class RolesController : Controller
-    {
-        public readonly RoleManager<IdentityRole> _rolemanager;
-        public RolesController(RoleManager<IdentityRole> rolemanager)
-        {
-            _rolemanager = rolemanager;
-        }
-        public IActionResult Index()
-        {
-            var roles = _rolemanager.Roles;
-            return View( "Pages/Roles.cshtml", roles);
-        }
+//namespace kiyotaka.Web.Controllers
+//{
+//    public class RolesController : Controller
+//    {
+//        public readonly RoleManager<IdentityRole> _rolemanager;
+//        public RolesController(RoleManager<IdentityRole> rolemanager)
+//        {
+//            _rolemanager = rolemanager;
+//        }
+//        public IActionResult Index()
+//        {
+//            var roles = _rolemanager.Roles;
+//            return View( "Pages/Roles.cshtml", roles);
+//        }
 
-        [HttpGet]
-        public IActionResult Create()
-        {
-            return View();
-        }
+//        [HttpGet]
+//        public IActionResult Create()
+//        {
+//            return View();
+//        }
 
-        [HttpPost]
-        public async Task<IActionResult> Create(IdentityRole model)
-        {
-            if (!_rolemanager.RoleExistsAsync(model.Name).GetAwaiter().GetResult())//Checks whether the role is already there
-            {
-                _rolemanager.CreateAsync(new IdentityRole(model.Name)).GetAwaiter().GetResult();//If not it is created
-            }
+//        [HttpPost]
+//        public async Task<IActionResult> Create(IdentityRole model)
+//        {
+//            if (!_rolemanager.RoleExistsAsync(model.Name).GetAwaiter().GetResult())//Checks whether the role is already there
+//            {
+//                _rolemanager.CreateAsync(new IdentityRole(model.Name)).GetAwaiter().GetResult();//If not it is created
+//            }
             
-            return RedirectToAction("Index");
-        }
-    }
-}
+//            return RedirectToAction("Index");
+//        }
+//    }
+//}
